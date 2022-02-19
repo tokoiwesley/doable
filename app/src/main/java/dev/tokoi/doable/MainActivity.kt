df54139720e6a54466cd42566776735bcc2389e5
Wesley Tokoi <tokoiwesley@gmail.com>
2022-02-19 23:17:36 +0300

    Initial commit

---
+++ app/src/main/java/dev/tokoi/doable/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.tokoi.doable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
